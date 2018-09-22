f95f5e11940c1e11edafd8825995009c034fb653
Ryan Davis <ryan.davis@bison.howard.edu>
2018-09-22 14:02:27 -0400

    Initial Commit

---
+++ parseChatTests/parseChatTests.swift
@@ -0,0 +1,34 @@
+//
+//  parseChatTests.swift
+//  parseChatTests
+//
+//  Created by Ryan Davis on 9/22/18.
+//  Copyright © 2018 Ryan Davis. All rights reserved.
+//
+
+import XCTest
+@testable import parseChat
+
+class parseChatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
