f4e84dafee2bb17380f4c93d5ea1982072c56e52
ryandvs11 <32751317+ryandvs11@users.noreply.github.com>
2018-10-02 14:42:19 -0400

    Update LoginViewController.swift

--- parseChat/LoginViewController.swift
+++ parseChat/LoginViewController.swift
@@ -1,8 +1,8 @@
 //
 //  LoginViewController.swift
-//  parseChat
+//  parseChatP2
 //
-//  Created by Ryan Davis on 9/23/18.
+//  Created by Ryan Davis on 9/28/18.
 //  Copyright © 2018 Ryan Davis. All rights reserved.
 //
 
@@ -13,75 +13,6 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    
-    
-    @IBAction func signInBtn(_ sender: Any) {
-        func loginUser() {
-            let username = usernameField.text ?? ""
-            let password = passwordField.text ?? ""
-            
-            if !checkTextFields() {
-                return
-            }
-            
-            PFUser.logInWithUsername(inBackground: username, password: password) {
-                (user: PFUser?, error: Error?) in
-                
-                if let error = error {
-                    print("User log in failed: \(error.localizedDescription)")
-                } else {
-                    print("User logged in successfully")
-                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
-                }
-                
-            }
-            
-        }
-    }
-    
-    @IBAction func signUpBtn(_ sender: Any) {
-        func registerUser() {
-            // init a user object
-            let newUser = PFUser()
-            
-            if !checkTextFields() {
-                return
-            }
-            
-            //set user properties
-            newUser.username = usernameField.text
-            newUser.password = passwordField.text
-            
-            newUser .signUpInBackground { (succeeded: Bool, error: Error?) in
-                if let error = error{
-                    print(error.localizedDescription)
-                    let alertController = UIAlertController(title: "Error", message: "Invalid sign up information", preferredStyle: .alert)
-                    
-                    let cancelAction = UIAlertAction(title:"cancel", style: .cancel){
-                        (action) in
-                    }
-                    
-                    alertController.addAction(cancelAction)
-                    
-                    let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
-                    }
-                    alertController.addAction(OKAction)
-                    
-                    self.present(alertController, animated: true) {
-                        // optional code for what happens after the alert controller has finished presenting
-                    }
-                    
-                }
-                else{
-                    print("Successfully signed up")
-                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
-                }
-            }
-            
-        }
-
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -96,6 +27,7 @@ class LoginViewController: UIViewController {
         if username.isEmpty {
             let alertController = UIAlertController(title: "Missing", message: "Enter Username Please", preferredStyle: .alert)
             let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+                // handle case of user canceling. Doing nothing will dismiss the view.
             }
             
             alertController.addAction(cancelAction)
@@ -109,6 +41,7 @@ class LoginViewController: UIViewController {
         if password.isEmpty {
             let alertController = UIAlertController(title: "Missing", message: "Enter Password Please", preferredStyle: .alert)
             let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+                // handle case of user canceling. Doing nothing will dismiss the view.
             }
             
             alertController.addAction(cancelAction)
@@ -122,8 +55,57 @@ class LoginViewController: UIViewController {
         return true
     }
     
-
-
+    @IBAction func onSignIn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+                
+                let alertController = UIAlertController(title: "Error", message: "Invalid sign up information", preferredStyle: .alert)
+                
+                //create a cancel action
+                let cancelAction = UIAlertAction(title:"Cancel", style: .cancel){
+                    (action) in
+                }
+                
+                // add the cancel action to the alertController
+                alertController.addAction(cancelAction)
+                
+                // create an OK action
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                    // handle response here.
+                }
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
+                
+                self.present(alertController, animated: true) {
+                    // optional code for what happens after the alert controller has finished presenting
+                }
+            }
+            else {
+                print("User logged in successfully")
+                
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                // display view controller that needs to shown after successful login
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                // manually segue to logged in view
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
